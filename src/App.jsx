b7633176101d653bf46f5c6b8bb2e92be3dec18e
Brigus <brigus@users.noreply.github.com>
2024-07-07 22:09:06 -0400

    Lists in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-const title = 'React';
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abromov, Andrew Clarke',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>My hacker stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" /> 
+
+      <hr/>
+
+      <ul>
+        {list.map(function(item) {
+          return <li key={item.objectID}>
+                   <span>
+                    <a href={item.url}>{item.title}</a><br/>
+                   </span>
+                   <span>{item.author}</span><br/>
+                   <span>{item.num_comments}</span><br/>
+                   <span>{item.points}</span><br/>
+                 </li>;
+        })}
+      </ul>
     </div>
   )
 }
