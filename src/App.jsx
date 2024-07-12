869deef3fd5da72c82e922ceaed4622f7048a3cb
Brigus <brigus@users.noreply.github.com>
2024-07-12 16:11:55 -0400

    Add handler functions

--- src/App.jsx
+++ src/App.jsx
@@ -44,11 +44,24 @@ const List = () => (
   </ul>
 );
 
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </div>
-);
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  };
+
+  const handleBlur = (event) => {
+    console.log(event);
+  }
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} onBlur={handleBlur} />
+    </div>
+  );
+};
 
 export default App;
