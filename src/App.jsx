0756a7f15f983603a3dde41afb1c7e5bf217eaa1
Brigus <brigus@users.noreply.github.com>
2024-07-12 15:04:49 -0400

    Refactor to use arrow functions with consise bodies -- no return statement

--- src/App.jsx
+++ src/App.jsx
@@ -17,30 +17,38 @@ const list = [
   },
 ]
 
-function App() {
-  return (
-    <div>
-      <h1>My hacker stories</h1>
+const App = () => (
+  <div>
+    <h1>My hacker stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" /> 
+    <Search />
 
-      <hr/>
+    <hr/>
 
-      <ul>
-        {list.map(function(item) {
-          return <li key={item.objectID}>
-                   <span>
-                    <a href={item.url}>{item.title}</a><br/>
-                   </span>
-                   <span>{item.author}</span><br/>
-                   <span>{item.num_comments}</span><br/>
-                   <span>{item.points}</span><br/>
-                 </li>;
-        })}
-      </ul>
-    </div>
-  )
-}
+    <List />
+  </div>
+);
+
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a><br/>
+        </span>
+        <span>{item.author}</span><br/>
+        <span>{item.num_comments}</span><br/>
+        <span>{item.points}</span><br/>
+      </li>
+    ))}
+  </ul>
+);
+
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
 
 export default App;
